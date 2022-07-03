f3b81df8d48210a02a394020432dbaaae308484d
Jean Francois Munyaneza <mjeanfrancois360@gmail.com>
2022-07-03 12:53:36 +0200

    Fix file extension issue

--- src/file-upload/file-upload.controller.ts
+++ src/file-upload/file-upload.controller.ts
@@ -9,40 +9,24 @@ import {
 } from '@nestjs/common';
 import { Express } from 'express';
 import { FileInterceptor } from '@nestjs/platform-express';
-
-// @Controller('file-upload')
-// export class FileUploadController {
-//   @Post()
-//   @UseInterceptors(
-//     FileInterceptor('file', {
-//       storage: diskStorage({
-//         destination: './uploads',
-//         filename: (req, file, cb) => {
-//           const customFileName = uuidv4(),
-//             fileExtension = file.originalname.split('.')[1]; // get file extension from original file name
-//           cb(null, customFileName + '.' + fileExtension);
-//         },
-//       }),
-//     }),
-//   )
-//   uploadFile(@UploadedFile() file: Express.Multer.File) {
-//     return {
-//       file: file.filename,
-//       status: '201',
-//       message: 'File uploaded successfully',
-//     };
-//   }
-
-//   @Get(':imgPath')
-//   seeUploadedFile(@Param('imgPath') image: string, @Res() res: any) {
-//     return res.sendFile(image, { root: 'uploads' });
-//   }
-// }
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('file-upload')
 export class FileUploadController {
   @Post()
-  @UseInterceptors(FileInterceptor('file', { dest: './uploads' }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const customFileName = uuidv4(),
+            fileExtension = file.originalname.split('.')[1]; // get file extension from original file name
+          cb(null, customFileName + '.' + fileExtension);
+        },
+      }),
+    }),
+  )
   uploadSingle(@UploadedFile() file: Express.Multer.File) {
     return {
       file: file.filename,
